6c57886bf3be00c48348f8bb4e1e89b9b8b3d4d5
JasonStaubach <jbstaubach@gmail.com>
2023-06-27 16:56:31 -0400

    Started making fish

---
+++ src/fish.js
@@ -0,0 +1,15 @@
+export default class Fish{
+
+    const FISHCLASS = [
+        "catfish",
+        "dogfish",
+        "tarpon",
+        "mackerel",
+        "SeaLion"
+    ]
+    constructor(){
+        switch(let x = Math.floor(Math.random()*1000)){
+            case 
+        }
+    }
+}
