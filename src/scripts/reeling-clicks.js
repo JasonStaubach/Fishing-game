54605795576a96cb55fb73184bb753d12eebd216
JasonStaubach <jbstaubach@gmail.com>
2023-07-03 10:40:04 -0400

    circle is now round but still off-screen (CSS stretches canvas, resize in html)

--- src/scripts/reeling-clicks.js
+++ src/scripts/reeling-clicks.js
@@ -3,7 +3,10 @@ export function timingMinigame(fish){
     minigame.classList.add('minigame')
     minigame.setAttribute('id','minigame-canvas')
     document.getElementById("board-container").appendChild(minigame)
-    minigame.setAttribute('top', `${(-1100 + fish.pos[0])}px`)
+    minigame.setAttribute('width', '200px')
+    minigame.setAttribute('height', '200px')
+    minigame.setAttribute('position', 'relative')
+    minigame.setAttribute('top', '-600px') //`${(-1100 + fish.pos[0])}px`)
 
 
     const minigameCanvas = document.getElementById('minigame-canvas')     //print background on background canvas
@@ -12,9 +15,10 @@ export function timingMinigame(fish){
     debugger
 
     for(let i = 0; i < fish.reels; i++){       
-        let gradient = mgctx.createRadialGradient(50, 50, 10, 50, 50, 90);      //(x, y, innerRadius, x, y, outerRadius)
-        gradient.addColorStop(0, 'green');
-        gradient.addColorStop(1, 'white');
+        let gradient = mgctx.createRadialGradient(100, 100, 10, 100, 100, 90);      //(x, y, innerRadius, x, y, outerRadius)
+        gradient.addColorStop(1, 'green');
+        gradient.addColorStop(0.5, 'blue');
+        gradient.addColorStop(0, 'red')
         mgctx.arc(100,100, 50, 0, 2 * Math.PI)          //(x, y, radius, start, end);
         mgctx.fillStyle = gradient;
         mgctx.fill();
